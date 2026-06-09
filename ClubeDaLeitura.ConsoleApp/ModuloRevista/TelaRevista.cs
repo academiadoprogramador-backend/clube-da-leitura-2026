02307e5b3b6af97f3c51c7e18aaf07605d1b8c44
Tiago Santini <tiagosantiniathayde@outlook.com>
2026-06-09 20:49:12 -0300

    Extrai método Excluir() para TelaBase

--- ClubeDaLeitura.ConsoleApp/ModuloRevista/TelaRevista.cs
+++ ClubeDaLeitura.ConsoleApp/ModuloRevista/TelaRevista.cs
@@ -17,30 +17,6 @@ public class TelaRevista : TelaBase
         this.repositorioCaixa = repositorioCaixa;
     }
 
-    public void Excluir()
-    {
-        Console.WriteLine("---------------------------------");
-        Console.WriteLine("Exclusão de Revista");
-        Console.WriteLine("---------------------------------");
-
-        // 1. Visualizar revistas cadastradas e obter o ID que deseja excluir
-        VisualizarTodos(false);
-
-        Console.WriteLine("---------------------------------");
-
-        Console.Write("Digite o ID do registro que deseja excluir: ");
-        int idSelecionado = Convert.ToInt32(Console.ReadLine());
-
-        // 2. Excluir a revista no repositório
-        repositorioRevista.Excluir(idSelecionado);
-
-        Console.WriteLine("---------------------------------");
-        Console.WriteLine($"O registro de ID \"{idSelecionado}\" foi excluído com sucesso!");
-        Console.WriteLine("---------------------------------");
-        Console.WriteLine("Digite ENTER para continuar");
-        Console.ReadLine();
-    }
-
     public override void VisualizarTodos(bool deveExibirCabecalho)
     {
         if (deveExibirCabecalho)
