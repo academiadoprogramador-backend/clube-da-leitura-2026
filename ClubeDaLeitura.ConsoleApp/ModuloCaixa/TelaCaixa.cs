b13562485f7a90b44b85252803bbecfe6bd4b7c9
Tiago Santini <tiagosantiniathayde@outlook.com>
2026-06-09 19:39:50 -0300

    Adiciona TelaBase e extrai ObterOpcaoMenu()

--- ClubeDaLeitura.ConsoleApp/ModuloCaixa/TelaCaixa.cs
+++ ClubeDaLeitura.ConsoleApp/ModuloCaixa/TelaCaixa.cs
@@ -3,33 +3,18 @@ using ClubeDaLeitura.ConsoleApp.ModuloRevista;
 
 namespace ClubeDaLeitura.ConsoleApp.ModuloCaixa;
 
-public class TelaCaixa
+public class TelaCaixa : TelaBase
 {
     private readonly RepositorioCaixa repositorioCaixa;
     private readonly RepositorioRevista repositorioRevista;
 
-    public TelaCaixa(RepositorioCaixa repositorioCaixa, RepositorioRevista repositorioRevista)
+    public TelaCaixa(
+        string nomeEntidade,
+        RepositorioCaixa repositorioCaixa,
+        RepositorioRevista repositorioRevista) : base(nomeEntidade)
     {
         this.repositorioCaixa = repositorioCaixa;
         this.repositorioRevista = repositorioRevista;
-    }
-
-    public string? ObterOpcaoMenu()
-    {
-        Console.WriteLine("---------------------------------");
-        Console.WriteLine("Gestão de Caixas");
-        Console.WriteLine("---------------------------------");
-        Console.WriteLine("1 - Cadastrar caixa");
-        Console.WriteLine("2 - Editar caixa");
-        Console.WriteLine("3 - Excluir caixa");
-        Console.WriteLine("4 - Visualizar caixas");
-        Console.WriteLine("S - Sair");
-        Console.WriteLine("---------------------------------");
-        Console.Write("> ");
-
-        string? opcaoMenuInterno = Console.ReadLine()?.ToUpper();
-
-        return opcaoMenuInterno;
     }
 
     public void Cadastrar()
--- ClubeDaLeitura.ConsoleApp/ModuloRevista/Revista.cs
+++ ClubeDaLeitura.ConsoleApp/ModuloRevista/Revista.cs
@@ -29,14 +29,6 @@ public class Revista : EntidadeBase
         Caixa = caixa;
     }
 
-    public void Atualizar(Revista revistaAtualizada)
-    {
-        Titulo = revistaAtualizada.Titulo;
-        NumeroEdicao = revistaAtualizada.NumeroEdicao;
-        AnoPublicacao = revistaAtualizada.AnoPublicacao;
-        Caixa = revistaAtualizada.Caixa;
-    }
-
     // substituição de método
     public override void Atualizar(EntidadeBase entidadeAtualizada)
     {
