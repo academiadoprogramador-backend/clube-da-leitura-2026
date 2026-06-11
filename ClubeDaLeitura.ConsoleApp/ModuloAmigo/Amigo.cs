866f8fbc6ce4714015dcd7d85bd5e01e9d620dfa
Tiago Santini <tiagosantiniathayde@outlook.com>
2026-06-11 19:51:20 -0300

    Adiciona módulo de Amigos

---
+++ ClubeDaLeitura.ConsoleApp/ModuloAmigo/Amigo.cs
@@ -0,0 +1,29 @@
+using ClubeDaLeitura.ConsoleApp.Compartilhado;
+using ClubeDaLeitura.ConsoleApp.Utilidades;
+
+namespace ClubeDaLeitura.ConsoleApp.ModuloAmigo;
+
+public class Amigo : EntidadeBase
+{
+    public string Nome { get; private set; }
+    public string NomeResponsavel { get; private set; }
+    public string Telefone { get; private set; }
+
+    public Amigo(string nome, string nomeResponsavel, string telefone)
+    {
+        Id = GeradorIds.ObterIdAmigo();
+
+        Nome = nome;
+        NomeResponsavel = nomeResponsavel;
+        Telefone = telefone;
+    }
+
+    public override void Atualizar(EntidadeBase entidadeAtualizada)
+    {
+        Amigo amigoAtualizado = (Amigo)entidadeAtualizada;
+
+        Nome = amigoAtualizado.Nome;
+        NomeResponsavel = amigoAtualizado.NomeResponsavel;
+        Telefone = amigoAtualizado.Telefone;
+    }
+}
